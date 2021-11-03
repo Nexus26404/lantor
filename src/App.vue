8a99c794cb31562f1f2828e5ccd305d0dafef5cd
Nexus <1482763380@qq.com>
2021-11-03 22:30:36 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tabbar :title="msg"></tabbar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Tabbar from './components/Tabbar.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name:'',
+        components: {
+            Tabbar
+        },
+        data(){
+            return {
+                msg: 'Hao a you'
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
